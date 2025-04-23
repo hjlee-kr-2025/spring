cc490b55841333caf3c501c914d022d0c2beb00c
hjlee <hjlee-kr@naver.com>
2025-04-23 17:10:30 +0900

    mybatis-spring(v2.1.2): spring5이하에서 사용

---
+++ gyshop/src/test/java/com/gyshop/main/persistence/TimeMapperTests.java
@@ -0,0 +1,27 @@
+package com.gyshop.main.persistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.gyshop.mapper.TimeMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j2
+public class TimeMapperTests {
+
+	@Setter(onMethod_ = @Autowired)
+	private TimeMapper timeMapper;
+	
+	@Test
+	public void testGetTime() {
+		log.info(timeMapper.getClass().getName());
+		log.info(timeMapper.getTime());
+	}
+}
